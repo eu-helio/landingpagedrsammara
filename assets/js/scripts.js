efa8c9571ca980a377251e9be433f4c3a2b1d250
eu-helio <helio2rn1@gmail.com>
2025-03-12 17:28:24 -0300

    commit inicial com site apenas no mobile, faltando insetir os media querie, finalizar o js para o media, deploy

---
+++ assets/js/scripts.js
@@ -0,0 +1,42 @@
+const inicio = document.getElementById('inicio')
+
+const perguntas = document.getElementById('perguntas')
+
+const espec = document.getElementById('espec')
+
+const valores = document.getElementById('valores')
+
+const contato = document.getElementById('contato')
+
+
+function rol1() {
+    window.scrollTo (0, 0)
+ }
+
+
+
+function rol2() {
+    window.scrollTo (0, 100)
+ }
+
+
+
+
+function rol3() {
+    window.scrollTo (0, 500)
+ }
+
+
+
+
+function rol4() {
+    window.scrollTo (0, 850)
+ }
+
+
+
+
+
+ function rol5() {
+    window.scrollTo (0, 1000)
+ }
